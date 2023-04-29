88680072ef1c5e14d19aa72d60b5a6751b07c902
Zeynep Kaya <zk.14.hk@gmail.com>
2023-04-29 12:35:30 +0300

    yeni

---
+++ src/atmProjesi.java
@@ -0,0 +1,29 @@
+public class atmProjesi {
+
+    public static void main(String[] args) {
+
+        System.out.println("bu bir atm projesidir");
+
+//versiyon 1
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
