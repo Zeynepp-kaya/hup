adc742d655dfee131c669c1b29fca88d321b0a57
Zeynep Kaya <zk.14.hk@gmail.com>
2023-05-01 01:23:36 +0300

    ekledim

--- src/atmProjesi.java
+++ src/atmProjesi.java
@@ -3,14 +3,16 @@ public class atmProjesi {
     public static void main(String[] args) {
 
         System.out.println("bu birr atm projesidir");
+
+
+        System.out.println("ekledim kodlar");
     }
 
 
 //nazende branch kodlarini yazdi
 //nazende branchine kodlar yazildi
 
-//
-
+//yeni isler yeni kodlar ekledim
 
 
 
