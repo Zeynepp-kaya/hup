ff0160dc6540676f8528c090fac693212ec188ac
Zeynep Kaya <zk.14.hk@gmail.com>
2023-05-02 18:50:25 +0300

    3

--- src/hesapclasi.java
+++ src/hesapclasi.java
@@ -4,10 +4,13 @@ public class hesapclasi {
     public static void main(String[] args) {
 
         System.out.println("kodlar yazildi");
+
         System.out.println("zeynep branch de yenni kodlar 3");
 
+        System.out.println("zeynep branch de yapilan islemler lokal");
 
 
+        System.out.println("zeynep branch yapilan islemler 2");
     }
 
 
