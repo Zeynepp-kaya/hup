c7b7eb725b2f46c9bf47afe15f9c9c10d37b1d38
Zeynep Kaya <zk.14.hk@gmail.com>
2023-05-02 18:45:02 +0300

    kodlar 3

--- src/hesapclasi.java
+++ src/hesapclasi.java
@@ -4,7 +4,7 @@ public class hesapclasi {
     public static void main(String[] args) {
 
         System.out.println("kodlar yazildi");
-
+        System.out.println("zeynep branch de yenni kodlar 3");
 
 
 
